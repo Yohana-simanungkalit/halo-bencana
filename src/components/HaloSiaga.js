414d4b190b7da15df77c48436a03df641cd3d9ed
unknown <you@example.com>
2026-03-20 23:40:22 +0700

    feat: maps

--- src/components/HaloSiaga.js
+++ src/components/HaloSiaga.js
@@ -2,6 +2,8 @@
 import { useEffect, useRef, useState } from "react";
 import QuickActions from "./QuickActions";
 import Message from "./Message";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 export default function HaloSiaga() {
     // const API_KEY = "AIzaSyCvv-laH6xQN1FRsrrOWlJiwShnu0kgB6U";
@@ -10,12 +12,29 @@ export default function HaloSiaga() {
     //     apiVersion: "v1alpha"
     // });
     const [messages, setMessages] = useState([
-        { role: "bot", text: "Halo! Saya Bot Halo Bencana. Saya siap membantu informasi saat bencana." }
+        { role: "bot", text: "Saya Bot HaloSiaga. Saya siap membantu informasi saat bencana." }
     ]);
 
     const [input, setInput] = useState("");
     const [loading, setLoading] = useState(false);
     const chatEndRef = useRef(null);
+
+    const [showMap, setShowMap] = useState(false);
+    const [userLocation, setUserLocation] = useState(null);
+    const [locationDenied, setLocationDenied] = useState(false)
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(
+            (pos) => {
+                setUserLocation([pos.coords.latitude, pos.coords.longitude]);
+            },
+            (err) => {
+                if (err.code === 1) {
+                    setLocationDenied(true);
+                }
+            }
+        );
+    }, []);
 
     useEffect(() => {
         chatEndRef.current?.scrollIntoView({ behavior: "smooth" });
@@ -37,14 +56,13 @@ export default function HaloSiaga() {
             const res = await fetch("https://openrouter.ai/api/v1/chat/completions", {
                 method: "POST",
                 headers: {
-                    "Authorization": "Bearer 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",
+                    "Authorization": "Bearer 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",
                     "Content-Type": "application/json",
-                    "HTTP-Referer": "http://localhost:3000", 
-                    "X-Title": "SiagaAI App" 
+                    "HTTP-Referer": "http://localhost:3000",
+                    "X-Title": "Halo Siaga App"
                 },
                 body: JSON.stringify({
-                    model: "openrouter/auto",
-                    // model: "meta-llama/llama-3-8b-instruct:free",
+                    model: "nvidia/nemotron-3-super-120b-a12b:free",
                     messages: [
                         {
                             role: "system",
@@ -66,7 +84,7 @@ export default function HaloSiaga() {
 
             const botText =
                 data?.choices?.[0]?.message?.content ||
-                "Maaf, tidak ada respon dari Halo Bencana.";
+                "Maaf, tidak ada respon dari Halo Siaga.";
 
             setMessages(prev => [...prev, { role: "bot", text: botText }]);
 
@@ -92,12 +110,60 @@ export default function HaloSiaga() {
         <div className="container">
 
             <div className="header">
-                <h2>Halo Bencana</h2>
-                <p>Asisten Kesiapsiagaan Bencana</p>
+                <h3>HaloSiaga, Siap membantu anda!</h3>
             </div>
 
-            <QuickActions sendMessage={sendMessage} />
+            {showMap && (
+                <div className="map-container">
+                    <button
+                        className="close-map-btn"
+                        onClick={() => setShowMap(false)}
+                    >
+                        ✖
+                    </button>
+                    <MapContainer
+                        center={userLocation || [-6.2, 106.8]}
+                        zoom={13}
+                        style={{ height: "200px", borderRadius: "15px", margin: "10px" }}
+                    >
+                        <TileLayer
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
 
+                        {userLocation && (
+                            <Marker position={userLocation}>
+                                <Popup>📍 Lokasi Anda</Popup>
+                            </Marker>
+                        )}
+
+                    </MapContainer>
+                    {locationDenied && (
+                        <div className="map-overlay">
+                            <div className="map-popup">
+                                <button
+                                    className="close-btn"
+                                    onClick={() => setLocationDenied(false)}
+                                >
+                                    ✖
+                                </button>
+                                <h4>Lokasi Tidak Diizinkan</h4>
+                                <p>Aktifkan lokasi untuk melihat posisi Anda.</p>
+                            </div>
+                        </div>
+                    )}
+
+                </div>
+            )}
+
+            {!showMap && <button
+                className="map-btn"
+                onClick={() => {
+                    setShowMap(true);
+                }}
+            >
+                🗺️ Lihat Lokasi Saya
+            </button>}
+            <QuickActions sendMessage={sendMessage} />
             <div className="chat-box">
 
                 {messages.map((msg, index) => (
@@ -127,7 +193,6 @@ export default function HaloSiaga() {
                 </button>
 
             </div>
-
-        </div>
+        </div >
     );
 }
